027977109418d0fee6a019d2d69794bc907e8f92
Xue Li <xuelimiis@gmail.com>
2024-09-25 19:38:29 -0700

    [feat] add flash messages for form submit

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,23 @@
 'use client';
 import React from 'react';
+import { useRouter } from "next/navigation";
+
+import { ToastContext } from "../../../../components/ToastProvider";
 
 function ContactPage() {
+  const { createToast } = React.useContext(ToastContext);
+  const router = useRouter();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push("/exercises/02-flash-messages");
+    createToast(
+      `Your message was received. We'll get back to you shortly!`,
+      "success"
+    );
+  };
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import './styles.css';
 
+import ToastProvider from "../../../components/ToastProvider";
+import ToastShelf from "../../../components/ToastShelf";
+
 function FlashMsgLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ToastProvider>
+      <html lang="en">
+        <body>
+          {children}
+          <ToastShelf />
+        </body>
+      </html>
+    </ToastProvider>
   );
 }
 
